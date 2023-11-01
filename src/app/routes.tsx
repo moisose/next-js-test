688ff3bf1cccb8d7755ed98ef87e46b135b0bad6
Moises Solano <90010406+moisose@users.noreply.github.com>
2023-11-01 01:49:35 -0600

    Update routes.tsx

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -3,32 +3,32 @@ const port = 8080;
 const urlAPI = "52.53.155.96";
 
 // STORE
-export const getCatogue = `${urlAPI}:${port}/getCatalogue`;
-export const getProduct = `${urlAPI}:${port}/getProduct`;
-export const getProductByName = `${urlAPI}:${port}/getProductByName`;
-export const addProduct = `${urlAPI}:${port}/addProduct`;
-export const modifyProduct = `${urlAPI}:${port}/updateProduct`;
+export const getCatogue = `52.53.155.96:${port}/getCatalogue`;
+export const getProduct = `52.53.155.96:${port}/getProduct`;
+export const getProductByName = `52.53.155.96:${port}/getProductByName`;
+export const addProduct = `52.53.155.96:${port}/addProduct`;
+export const modifyProduct = `52.53.155.96:${port}/updateProduct`;
 
-// export const updateCart = `${urlAPI}:${port}/updateCart`;
-export const deleteProduct = `${urlAPI}:${port}/deleteProduct`;
+// export const updateCart = `52.53.155.96:${port}/updateCart`;
+export const deleteProduct = `52.53.155.96:${port}/deleteProduct`;
 
 // FILTER
-export const getCategories = `${urlAPI}:${port}/getCategories`;
+export const getCategories = `52.53.155.96:${port}/getCategories`;
 
 // GALLERY
-export const getContent = `${urlAPI}:${port}/getContent`;
-export const getGallery = `${urlAPI}:${port}/getAllContent`;
-export const addContent = `${urlAPI}:${port}/addContent`;
-export const modifyContent = `${urlAPI}:${port}/updateContent`;
-export const deleteContent = `${urlAPI}:${port}/deleteContent`;
+export const getContent = `52.53.155.96:${port}/getContent`;
+export const getGallery = `52.53.155.96:${port}/getAllContent`;
+export const addContent = `52.53.155.96:${port}/addContent`;
+export const modifyContent = `52.53.155.96:${port}/updateContent`;
+export const deleteContent = `52.53.155.96:${port}/deleteContent`;
 
 // USER
-export const getUserInfo = `${urlAPI}:${port}/infoUser`;
-export const getHistory = `${urlAPI}:${port}/getPurchaseHistory`;
-export const getCart = `${urlAPI}:${port}/getCart`;
-export const updateCart = `${urlAPI}:${port}/updateCart`;
-export const makePurchase = `${urlAPI}:${port}/makePurchase`;
-export const addUser = `${urlAPI}:${port}/addUser`;
-export const updatePurchaseState = `${urlAPI}:${port}/updatePurchaseState`;
+export const getUserInfo = `52.53.155.96:${port}/infoUser`;
+export const getHistory = `52.53.155.96:${port}/getPurchaseHistory`;
+export const getCart = `52.53.155.96:${port}/getCart`;
+export const updateCart = `52.53.155.96:${port}/updateCart`;
+export const makePurchase = `52.53.155.96:${port}/makePurchase`;
+export const addUser = `52.53.155.96:${port}/addUser`;
+export const updatePurchaseState = `52.53.155.96:${port}/updatePurchaseState`;
 
 // USER ROUTES
